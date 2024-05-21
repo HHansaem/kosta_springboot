77133e19b3d3719f31173a89457b30d0dadc21fa
HHansaem <hhs01071331@gmail.com>
2024-05-21 10:10:57 +0900

    spring-security 프로젝트 생성

---
+++ securityex/src/main/java/com/kosta/sec/SecurityexApplication.java
@@ -0,0 +1,13 @@
+package com.kosta.sec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityexApplication.class, args);
+	}
+
+}
